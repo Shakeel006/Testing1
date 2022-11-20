d4934dabd1b265a97ef3fb42df0aa1abc0b01ef8
shakeel006 <smartshakeel25@gmail.com>
2022-11-20 13:54:02 +0530

    Testing

---
+++ src/test/java/org/testng/Sample.java
@@ -0,0 +1,32 @@
+package org.testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	static WebDriver driver;
+	@Parameters("browser")
+	@Test
+	private void m1(String browser) {
+if(browser.equals("chrome")) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+
+	driver.get("https://adactinhotelapp.com/");
+
+}
+WebElement txtuser = driver.findElement(By.id("username"));
+txtuser.sendKeys("Shakeels");
+WebElement txtpass = driver.findElement(By.id("password"));
+txtpass.sendKeys("Ahmedsha006@");
+txtpass.getText();
+WebElement btnlogin = driver.findElement(By.className("login_button"));
+btnlogin.click();
+	}
+}
---
+++ src/test/java/org/testng/Test1.java
@@ -0,0 +1,12 @@
+package org.testng;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+private void m2() {
+	// TODO Auto-generated method stub
+System.out.println("sha");
+}
+
+}
